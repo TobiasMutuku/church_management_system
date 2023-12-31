42fbf3c8657760bfd4a95aa20c8a915a556ac9b6
Tobias Mutuku <113021484+TobiasMutuku@users.noreply.github.com>
2023-12-31 14:44:19 +0300

    Add files via upload
    changes

---
+++ pages/sample_101.php
@@ -0,0 +1,22 @@
+<?php require_once('../config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> SUGARS | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="Sugars is the culprit of all the chronic ailments.">
+<meta name="keywords" content="sugar, food, fructose,sucrose">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="SUGARS" />
+<meta property="og:description" content="Sugars is the culprit of all the chronic ailments." />
+<meta property="og:image" content="http://localhost/church_management/uploads/blog_uploads/banners/1_banner.jpg" />
+<meta property="og:url" content="http://localhost/church_management/pages/sample_101.php" />
+<?php require_once('../inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+<h2>SUGARS</h2>
+<input name="blog_id" value="c4ca4238a0b923820dcc509a6f75849b" type="hidden">
+<hr>
+<small><span class='text-muted'><i class='fa fa-calendar-day'></i> Published: Aug 17,2021 12:56 PM</span></small>
+<center><img src="http://localhost/church_management/uploads/blog_uploads/banners/1_banner.jpg" class="img-thumbnail img-banner" alt="http://localhost/church_management/pages/sample_101.php" /></center>
+Sugars is the culprit of all the chronic ailments.
+<?php include(base_app.'inc/body_block_end.php') ?>
---
+++ pages/sample_102.php
@@ -0,0 +1,22 @@
+<?php require_once('../config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> Saccos & MMF | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="Sacco are the best institutions to invest your money">
+<meta name="keywords" content="Sacco, money, MMF, investment, deposit">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Saccos & MMF" />
+<meta property="og:description" content="Sacco are the best institutions to invest your money" />
+<meta property="og:image" content="http://localhost/church_management/uploads/blog_uploads/banners/2_banner.jpg" />
+<meta property="og:url" content="http://localhost/church_management/pages/sample_102.php" />
+<?php require_once('../inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+<h2>Saccos & MMF</h2>
+<input name="blog_id" value="c81e728d9d4c2f636f067f89cc14862c" type="hidden">
+<hr>
+<small><span class='text-muted'><i class='fa fa-calendar-day'></i> Published: Aug 17,2021 01:57 PM</span></small>
+<center><img src="http://localhost/church_management/uploads/blog_uploads/banners/2_banner.jpg" class="img-thumbnail img-banner" alt="http://localhost/church_management/pages/sample_102.php" /></center>
+<h3 style="text-align: left; margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;">Sacco are the best institutions to invest your money</h3>
+<?php include(base_app.'inc/body_block_end.php') ?>
